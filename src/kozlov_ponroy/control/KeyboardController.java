e5c0ba6112f915b62ca91e39a44f7bb4745d6343
Loris <loris.ponroy@lilo.org>
2021-02-01 14:41:26 +0100

    controller

--- src/kozlov_ponroy/control/KeyboardController.java
+++ src/kozlov_ponroy/control/KeyboardController.java
@@ -1,5 +1,70 @@
 package kozlov_ponroy.control;
 
-public class KeyboardController {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import kozlov_ponroy.model.Etat;
+
+public class KeyboardController implements MouseListener, KeyListener {
+
+	private Etat etat ;
+
+	public KeyboardController(Etat etat) {
+		this.etat = etat;
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyChar()=='d') {
+			etat.setPlayerX(etat.getPlayerX()+1);
+		}
+		if(e.getKeyChar()=='q') {
+			etat.setPlayerX(etat.getPlayerX()-1);
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
--- src/kozlov_ponroy/model/Etat.java
+++ src/kozlov_ponroy/model/Etat.java
@@ -1,5 +1,20 @@
 package kozlov_ponroy.model;
 
 public class Etat {
+	public static final int PLAYER_Y = 0;
+
+	private int playerX = 0;
+
+	public int getPlayerX() {
+		return playerX;
+	}
+
+	public void setPlayerX(int playerX) {
+		if(this.playerX<0) {
+			playerX=0;
+		}
+		this.playerX = playerX;
+	}
+
 
 }
