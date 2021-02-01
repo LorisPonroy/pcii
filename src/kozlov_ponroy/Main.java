b892821e2cfc0753183efc98761b2d88053e1913
Asey <fr.kozlov.antoine@gmail.com>
2021-02-01 14:31:58 +0100

    Affichage termine

--- src/kozlov_ponroy/Main.java
+++ src/kozlov_ponroy/Main.java
@@ -1,9 +1,24 @@
 package kozlov_ponroy;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+import kozlov_ponroy.model.Etat;
+import kozlov_ponroy.view.Affichage;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.print("AAAA");
+		JFrame fenetre = new JFrame("Exo 6");
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Affichage aff = new Affichage();
+        Etat etat = new Etat();
+        fenetre.add(aff);
+        fenetre.pack();
+        fenetre.setLocationRelativeTo(null);
+        fenetre.setVisible(true);
 	}
 
 }
--- src/kozlov_ponroy/view/Affichage.java
+++ src/kozlov_ponroy/view/Affichage.java
@@ -1,5 +1,30 @@
 package kozlov_ponroy.view;
 
-public class Affichage {
+import java.awt.Dimension;
+import java.awt.Graphics;
 
+import javax.swing.JPanel;
+
+public class Affichage extends JPanel{
+
+	private final int HAUTEUR = 400;
+    private final int LARGEUR = 800;
+    
+    public Affichage(){
+        /** Initialise la taille de la fenetre au lancement*/
+        setPreferredSize(new Dimension(LARGEUR, HAUTEUR));
+    }
+    
+    public int getHAUTEUR() {
+        return HAUTEUR;
+    }
+
+    public int getLARGEUR() {
+        return LARGEUR;
+    }
+    
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+    }
 }
