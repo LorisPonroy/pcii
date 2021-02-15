437eec43cda87ccd5fa8f0e98c1d9208884efc0c
Asey <fr.kozlov.antoine@gmail.com>
2021-02-15 14:42:39 +0100

    Route bande

--- src/kozlov_ponroy/view/Affichage.java
+++ src/kozlov_ponroy/view/Affichage.java
@@ -69,7 +69,7 @@ public class Affichage extends JPanel{
 			graphics2D.fillPolygon(x, y, x.length);
 			g.setColor(Color.yellow);
 			for(int j = 1 ; j < 4 ; j+=2) {
-				x[0] = p1.x - etat.getLargeurRoute(p1) / 2; y[0] = p1.y;
+				x[0] = p1.x - 5; y[0] = p1.y;
 				x[1] = p2.x - etat.getLargeurRoute(p2) / 2; y[1] = p2.y;
 				x[2] = p2.x + etat.getLargeurRoute(p2) / 2; y[2] = p2.y;
 				x[3] = p1.x + etat.getLargeurRoute(p1) / 2; y[3] = p1.y;
