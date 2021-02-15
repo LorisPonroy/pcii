ec17b954d73523adcdd72f5055537be8ecad4129
Asey <fr.kozlov.antoine@gmail.com>
2021-02-15 16:50:19 +0100

    qsifhs

--- src/kozlov_ponroy/view/Affichage.java
+++ src/kozlov_ponroy/view/Affichage.java
@@ -18,6 +18,7 @@ import java.util.List;
 import javax.swing.JPanel;
 
 import kozlov_ponroy.model.Etat;
+import kozlov_ponroy.model.Route;
 
 /**
  * Gère l'affichage dans le MVC
@@ -111,37 +112,27 @@ public class Affichage extends JPanel{
 				ctrl = new Point2D.Double(x[2], y[2]);
 				debut = new Point2D.Double(x[2] + (x[4]- x[2]) / 2, y[2] + (y[4]- y[2]) / 2);
 				courbeDroite.setCurve(debut,ctrl,fin);
-				graphics2D.draw(courbeDroite);
 				//closedCurve.append(courbeGauche, true);
 				//closedCurve.append(courbeDroite, true);
 				//closedCurve.lineTo(x[0] + (x[1]- x[0]) / 2, y[0] + (y[1]- y[0]) / 2);
 				//closedCurve.closePath();
+				graphics2D.draw(courbeDroite);
 				//graphics2D.fill(closedCurve);
 			}
 			g.setColor(Color.gray);
 			for(float j = 0 ; j < NB_BANDE ; j+=2) {
-				y[0] = (int)(p1.y + (p2.y - p1.y) / NB_BANDE * j);
-				x[0] = (int)(- etat.getFacteurElargissement(y[0])/10 + p1.x + (p2.x - p1.x) / NB_BANDE * j);
-				y[1] = (int)(p1.y + (p2.y - p1.y) / NB_BANDE * (j+1));
-				x[1] = (int)(- etat.getFacteurElargissement(y[1])/10 + p1.x + (p2.x - p1.x) / NB_BANDE * (j+1));
-				y[2] = (int)(p1.y + (p2.y - p1.y) / NB_BANDE * (j+1));
-				x[2] = (int)(+ etat.getFacteurElargissement(y[2])/10 + p1.x + (p2.x - p1.x) / NB_BANDE * (j+1));
-				y[3] = (int)(p1.y + (p2.y - p1.y) / NB_BANDE * j);
-				x[3] = (int)(+ etat.getFacteurElargissement(y[3])/10 + p1.x + (p2.x - p1.x) / NB_BANDE * j);
-				y[4] = (int)(p1.y + (p2.y - p1.y) / NB_BANDE * j);
-				x[4] = (int)(- etat.getFacteurElargissement(y[4])/10 + p1.x + (p2.x - p1.x) / NB_BANDE * j);
+				x[0] = (int)(- 5 + p1.x + (p2.x - p1.x) / NB_BANDE * j); 		y[0] = (int)(p1.y + (p2.y - p1.y) / NB_BANDE * j);
+				x[1] = (int)(- 5 + p1.x + (p2.x - p1.x) / NB_BANDE * (j+1));	y[1] = (int)(p1.y + (p2.y - p1.y) / NB_BANDE * (j+1));
+				x[2] = (int)(+ 5 + p1.x + (p2.x - p1.x) / NB_BANDE * (j+1)); 	y[2] = (int)(p1.y + (p2.y - p1.y) / NB_BANDE * (j+1));
+				x[3] = (int)(+ 5 + p1.x + (p2.x - p1.x) / NB_BANDE * j); 	 	y[3] = (int)(p1.y + (p2.y - p1.y) / NB_BANDE * j);
+				x[4] = (int)(- 5 + p1.x + (p2.x - p1.x) / NB_BANDE * j); 		y[4] = (int)(p1.y + (p2.y - p1.y) / NB_BANDE * j);
 				graphics2D.fillPolygon(x, y, x.length);
 			}
 		}
 
-		//Suppresion de la route au dessus de l'horizon
-		g.clearRect(0, 0, LARGEUR, etat.getHorizon());
+		//Affichage point de fuite (temp)
+		g.setColor(Color.RED);
 
-		graphics2D.drawImage(montagneImage, LARGEUR / 2 + etat.getPositionDecor(), etat.getHorizon() - 130, 500, 130, null);
-		graphics2D.drawImage(montagneImage, LARGEUR + etat.getPositionDecor(), etat.getHorizon() - 130, 500, 130, null);
-		graphics2D.drawImage(montagneImage, -50 + etat.getPositionDecor(), etat.getHorizon() - 130, 500, 130, null);
-
-		//Affichage du joueur
 		g.setColor(C_VAISSEAU);
 		g.fillOval(etat.getPlayerX(), etat.getHauteurJoueur()+etat.getTailleJoueur(), etat.getTailleJoueur(), 20);
 		if(etat.isRight()) {
@@ -152,10 +143,18 @@ public class Affichage extends JPanel{
 			graphics2D.drawImage(playerCenterImage, etat.getPlayerX(), etat.getPlayerY(), etat.getTailleJoueur(), etat.getTailleJoueur(), null);
 		}
 
+		//Suppresion de la route au dessus de l'horizon
+		g.clearRect(0, 0, LARGEUR, etat.getHorizon());
+
+		graphics2D.drawImage(montagneImage, LARGEUR / 2 + etat.getPositionDecor(), etat.getHorizon() - 130, 500, 130, null);
+		graphics2D.drawImage(montagneImage, LARGEUR + etat.getPositionDecor(), etat.getHorizon() - 130, 500, 130, null);
+		graphics2D.drawImage(montagneImage, -50 + etat.getPositionDecor(), etat.getHorizon() - 130, 500, 130, null);
+
+
 		g.setColor(Color.black);
 		g.drawString("Score", 20, 20);
 		g.drawString(etat.getScore(), 20, 40);
-
+		
 		g.drawString("Vitesse : " + (int)(700 / etat.getFacteurVitesse()) + " km/h", 20, 60);
 	}
 
